e6a2a2a0e0e0fc08a3a856c1c52888b4e996c518
gnardinosaur <github email address>
2019-11-02 10:04:58 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -9,7 +9,7 @@ class StudentsController < ApplicationController
   end
 
   def create
-    @student = Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
+    session[:first_name] = params[:student][:first_name]
 
     redirect_to new_student_path
   end
