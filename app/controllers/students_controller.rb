1ada129328d12c5ddc36d93aa2b34eed31071c8d
GeneHFang <ghl234@nyu.edu>
2019-11-05 10:44:51 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,4 +8,31 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
 
+
+  def new
+
+    @student = Student.new
+
+    render :new
+
+  end
+
+
+  def create
+    # byebug
+    @student = Student.create(student_params)
+    
+  
+    # @first_name = student_params[:first_name]
+
+    redirect_to students_path
+
+  end
+
+
+  def student_params
+    params.require(:student).permit(:first_name, :last_name)
+  end
+
+
 end
